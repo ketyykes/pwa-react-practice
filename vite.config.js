0709e136c49c70be45563ea13c8dd49bee9075ba
danny <xi4869@gmail.com>
2025-03-05 14:40:05 +0800

    feat: 設定 GitHub Pages 基礎路徑
    - 在 vite.config.js 中新增 base 路徑為 "/pwa-react-practice/"
    - 確保 Vite 專案在 GitHub Pages 能正確部署和連結資源

--- vite.config.js
+++ vite.config.js
@@ -59,6 +59,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/pwa-react-practice/",
 	plugins: [
 		react(),
 		// , VitePWA(pwaConfig)
